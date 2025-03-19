aac85ab363ae896c94fc939af8b7fb0cf9011281
Ben Ponto <ponto038@d.umn.edu>
2025-03-19 16:35:28 -0500

    added MVP file structure for planner page

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'planner_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to PlannerPage
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PlannerPage()),
+            );
+          },
+          child: Text('Go to Planner'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/planner_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlannerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext Context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Planner')),
+      body: Center(
+        child: Text('Planner page!'),
+      ),
+    );
+  }
+}
