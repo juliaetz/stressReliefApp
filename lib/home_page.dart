901fd9d4d1f3922a5bcc63e6c2d1202cca6ab0b0
Andrew Martini <mart6482@d.umn.edu>
2025-03-20 15:07:55 -0500

    Created a class to handle button creation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
 import 'planner_page.dart'; // Import the Planner Page
 
+class Button extends StatelessWidget{
+  const Button({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.height,
+  });
+
+  final IconData icon;
+  final String label;
+  final double? height;
+
+
+  @override
+  Widget build(BuildContext context){
+    return Positioned(
+      top: height, // Distance from the top
+      right: 125,
+      // Distance from the right
+      child: ElevatedButton.icon(
+        onPressed: () {
+          // Navigate to PlannerPage
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PlannerPage()),
+          );
+        },
+        icon: Icon(icon),
+        label: Text(label),
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(150,75),
+        ),
+      ),
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
   @override
@@ -12,25 +48,10 @@ class HomePage extends StatelessWidget {
           // Main content of the home page
           Center(child: Text('Welcome to the Home Page!')),
           // Planner button positioned at the top-right
-          Positioned(
-            top: 400, // Distance from the top
-            right: 125,
-            // Distance from the right
-            child: ElevatedButton.icon(
-              onPressed: () {
-                // Navigate to PlannerPage
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PlannerPage()),
-                );
-              },
-              icon: const Icon(Icons.calendar_month_outlined),
-              label: const Text('Planner'),
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(150,75),
-              ),
-            ),
-          ),
+          Button(icon: Icons.calendar_month_outlined, label: 'Planner', height: 400),
+          Button(icon: Icons.tag_faces, label: 'Mood Tracker', height: 500),
+          Button(icon: Icons.timer, label: 'Activity History', height: 600),
+
         ],
       ),
     );
