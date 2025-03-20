55e340ea675cd6874c87a599480ab986aab82eac
etzko009 <etzko009@d.umn.edu>
2025-03-20 16:12:06 -0500

    adding background picture and home button to history page

--- lib/view/history_component.dart
+++ lib/view/history_component.dart
@@ -25,9 +25,33 @@ class _HistoryPageState extends State<HistoryPage> implements HistoryView {
   @override
   Widget build(BuildContext Context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Activity/Mood History')),
-      body: Center(
-        child: Text('History Page'),
+      appBar: AppBar(
+        toolbarHeight: 80,
+        leading: FilledButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.house, color:Colors.white),
+          style: FilledButton.styleFrom(
+            shape: CircleBorder(side: BorderSide(color: Colors.white, width: 8)),
+            padding: EdgeInsets.all(5),
+            backgroundColor: Colors.amber,
+            foregroundColor: Colors.amber,
+          ),
+        ),
+        title: Text('Daily Activity and Mood History'),
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/history_background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+            child: Text("History Page"),
+        ), /* add child content here */
       ),
     );
   }
