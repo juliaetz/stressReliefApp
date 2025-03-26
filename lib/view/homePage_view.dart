1e5abba38d1b1491a12bfdb828eaf5282bce1858
Andrew Martini <mart6482@d.umn.edu>
2025-03-26 16:33:38 -0500

    Moved the button class from the old frontpage branch to the new one. Added a test button.

--- lib/view/homePage_view.dart
+++ lib/view/homePage_view.dart
@@ -1,15 +1,51 @@
+//import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'calendar_view.dart';
 import '../model/calendar_model.dart';
 import '../presenter/calendar_presenter.dart';
 import '../view/moodTrack_view.dart'; // Import Mood Tracker Page
 
+//class to help create buttons
+class Button extends StatelessWidget{
+  const Button({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
+
+  final IconData icon;
+  final String label;
+
+  //creates a button using an icon and a label as a
+  //currently all buttons go to the planner page as a placeholder
+  @override
+  Widget build(BuildContext context){
+    return ElevatedButton.icon(
+      onPressed: () {
+        // Navigate to PlannerPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => CalendarView(presenter: CalendarPresenter(CalendarModel({}))),
+        )
+        );
+      },
+      icon: Icon(icon),
+      label: Text(label),
+      //dictates the size of the button
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(150,75),
+      ),
+    );
+  }
+}
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final model = CalendarModel({}); //creating an instance of calender model
-
     final calPresenter = CalendarPresenter(model); //
+
 
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
@@ -17,8 +53,7 @@ class HomePage extends StatelessWidget {
         children: [
           // Main content of the home page
           Center(child: Text('Welcome to the Home Page!')),
-
-
+          Button(icon: Icons.calendar_month_outlined, label: 'Planner'),
 
           // Planner button positioned at the top-right
           Positioned(
