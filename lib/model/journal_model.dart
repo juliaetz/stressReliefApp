f6c4c9c15dca45e73aa32f4e7848bcfb78229493
Andrew Martini <mart6482@d.umn.edu>
2025-04-03 15:02:28 -0500

    Created the skeleton for a few methods

--- lib/model/journal_model.dart
+++ lib/model/journal_model.dart
@@ -3,4 +3,17 @@ import 'package:flutter/material.dart';
 class JournalModel{
     final Map<DateTime, List<String>> entries;
     JournalModel (this.entries);
+    final Map<DateTime, List<Map<String, dynamic>>> _entries = {};
+
+    void addEntry(String text, DateTime date, TimeOfDay time){
+
+    }
+
+    void deleteEntry(DateTime date, String entry){
+
+    }
+
+    void getEntries(){
+
+    }
 }
