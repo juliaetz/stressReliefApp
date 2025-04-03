9a70ab3f24b2deb941557b91b389278f3de418be
etzko009 <etzko009@d.umn.edu>
2025-04-03 15:17:15 -0500

    creating the UI for the idea card, button row, and page navigation bar

--- lib/view/selfcare_component.dart
+++ lib/view/selfcare_component.dart
@@ -34,20 +34,80 @@ class _SelfcarePageState extends State<SelfcarePage> implements SelfcareView {
         backgroundColor: Colors.deepPurple.shade200,
       ),
       body: Container(
+        width: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/purple_background.jpg"),
-            fit: BoxFit.cover,
+            fit: BoxFit.fill,
           ),
         ),
 
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("this is the self care idea page"),
+            buildIdeaCard(),
+            SizedBox(height: 5.0),
+            buildButtonRow()
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.deepPurple.shade700,
+        iconSize: 30.0,
+
+        selectedIconTheme: IconThemeData(color: Colors.white),
+        selectedFontSize: 23.0,
+        selectedItemColor: Colors.white,
+
+        unselectedIconTheme: IconThemeData(color: Colors.deepPurple.shade200),
+        unselectedFontSize: 18.0,
+        unselectedItemColor: Colors.white,
+
+        items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.lightbulb),
+                label: 'Ideas',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+          ],
+      ),
     );
+  }
+
+  Row buildButtonRow() {
+    return Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.favorite, size: 20.0,),
+                  label: Text('Love', style: TextStyle(fontSize: 18.0)),
+              ),
+              SizedBox(width: 10.0),
+              ElevatedButton(
+                  onPressed: (){},
+                  child: Text('Next', style: TextStyle(fontSize: 18.0)),
+              )
+            ],
+          );
+  }
+
+  Card buildIdeaCard() {
+    return Card(
+              color: Colors.deepPurple.shade700,
+              margin: EdgeInsets.only(left: 40.0, right: 40.0),
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                      'RANDOM SELF CARE IDEA HERE',
+                      style: TextStyle(fontSize: 25.0, color: Colors.white,),
+                      textAlign: TextAlign.center,
+                  )
+              )
+          );
   }
 
   FilledButton buildHomeButton() {
