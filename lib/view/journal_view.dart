bc33a481b23bde8809d968c1abd45a763bf3087a
Andrew Martini <mart6482@d.umn.edu>
2025-04-03 14:38:45 -0500

    fixed formatting of early journal page. changed the naming scheme of the home page view

--- lib/view/journal_view.dart
+++ lib/view/journal_view.dart
@@ -17,20 +17,22 @@ class _JournalViewState extends State<JournalView>{
 
 
   @override
-  Widget build(BuildContext Context){
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text('Journal'),
         backgroundColor: Colors.deepPurple.shade200,
       ),
 
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'placeholder journal text'
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                'placeholder journal text'
+            ),
+          ],
+        ),
       ),
     );
   }
