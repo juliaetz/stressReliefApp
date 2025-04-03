55266f3175d605e415354aafcc6c4c4e5945745d
Angela Danger <dange084@d.umn.edu>
2025-04-03 14:31:52 -0500

    added current date to mood tracker page

--- lib/view/moodTrack_view.dart
+++ lib/view/moodTrack_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stress_managment_app/presenter/moodTrack_components/moodTrack_tile.dart';
 import 'package:stress_managment_app/view/moodPie.dart';
+import 'package:intl/intl.dart';
 
 class MoodtrackPage extends StatefulWidget{
   const MoodtrackPage({super.key});
@@ -30,11 +31,27 @@ class _MoodtrackPageState extends State<MoodtrackPage> {
 
       body: ListView(
         children: [
+          SizedBox(height: 20),  // spacing
+
+          // CURRENT DATE
+          Center(
+            child: Text(
+              "Today is " + DateFormat("yMMMMd").format(DateTime.now()),
+              style: TextStyle(
+                color: Colors.deepPurple[800],
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+              ),
+            )
+          ),
           SizedBox(height: 20), // spacing
+
+
           // MOOD TRACKER FACE BUTTONS
           MoodtrackTile(),
-
           SizedBox(height: 20), // spacing
+
+
           // PIE CHART
           ListTile(
             subtitle: SizedBox(
@@ -43,8 +60,9 @@ class _MoodtrackPageState extends State<MoodtrackPage> {
               child: Moodpie()
             ),
           ),
-
           SizedBox(height: 30), // spacing
+
+
           // TEXT FIELD FOR USER TO ENTER NOTES (thoughts, feelings, etc.)
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
@@ -56,7 +74,7 @@ class _MoodtrackPageState extends State<MoodtrackPage> {
             ),
           ),
         ],
-      )
+      ),
 
 
     );
