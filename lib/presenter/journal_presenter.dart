20045b43296722c8645994223ebe9fea1be7da18
Andrew Martini <mart6482@d.umn.edu>
2025-04-09 17:03:48 -0500

    Created a function to add journal entries

--- lib/presenter/journal_presenter.dart
+++ lib/presenter/journal_presenter.dart
@@ -1,8 +1,16 @@
-import 'package:flutter/material.dart';
 import '../model/journal_model.dart';
+import '../view/journal_view.dart';
 
 class JournalPresenter{
-    final JournalModel model;
-    JournalPresenter(this.model);
+    final JournalView view;
+    JournalPresenter(this.view);
 
+    //creates a new journal entry
+    void addEntry(String text){
+        JournalModel entry = JournalModel(text: text, timestamp: DateTime.now());
+
+        //logs the journal entry being added
+        print('Journal Entry Saved: ${entry.text} at ${entry.timestamp}');
+
+    }
 }
