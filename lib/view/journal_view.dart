69639d46397b1cfb4fb0e0e0084afead1571d964
Andrew Martini <mart6482@d.umn.edu>
2025-04-10 14:28:13 -0500

    Fixed the Journal button on the home screen

--- lib/view/journal_view.dart
+++ lib/view/journal_view.dart
@@ -14,7 +14,21 @@ class JournalView extends StatefulWidget {
 }
 
 class _JournalViewState extends State<JournalView>{
+  final _controller = TextEditingController();
 
+  void addEntry(String text){
+    widget.presenter.addEntry(text);
+    _controller.clear();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Entry added!')),
+    );
+  }
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context){
@@ -35,13 +49,22 @@ class _JournalViewState extends State<JournalView>{
             SizedBox(
               width: 250,
               child: TextField(
+                controller: _controller,
                 decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'New Journal Entry'),
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 //onSubmitted: addEntry(),
               ),
-            )
-
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(
+                onPressed: (){
+                  if(_controller.text.trim().isNotEmpty){
+                    addEntry(_controller.text.trim());
+                  }
+                },
+                child: Text('Add Entry'),
+            ),
           ],
         ),
       ),
