f961a179fbb4bb73c40e5c5242454d3e384ca39e
ols00068 <ols00068@d.umn.edu>
2025-04-16 22:23:51 -0500

    Added google authentication and the base login/sign up/account pages

--- lib/view/homepage_view.dart
+++ lib/view/homepage_view.dart
@@ -1,4 +1,5 @@
 //import 'dart:ffi';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:stress_managment_app/presenter/history_presenter.dart';
 import 'package:stress_managment_app/presenter/journal_presenter.dart';
@@ -12,9 +13,8 @@ import '../model/calendar_model.dart';
 import '../presenter/calendar_presenter.dart';
 import 'mood_tracker_screen/mood_tracker_view.dart';
 
-
 //class to help create buttons
-class Button extends StatelessWidget{
+class Button extends StatelessWidget {
   const Button({
     super.key,
     required this.icon,
@@ -28,72 +28,89 @@ class Button extends StatelessWidget{
   //creates a button using an icon and a label as a
   //currently all buttons go to the planner page as a placeholder
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     //creates a button and assigns what page it goes to by checking its PageID value
     return ElevatedButton.icon(
       onPressed: () {
         //check the given page id, and move to the page corresponding to the id.
-        if(pageID == 1){
+        if (pageID == 1) {
           // Navigate to PlannerPage
           Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => CalendarView(presenter: CalendarPresenter(CalendarModel())),
-              )
-          );
-        }else if(pageID == 2){
+                builder: (context) =>
+                    CalendarView(presenter: CalendarPresenter(CalendarModel())),
+              ));
+        } else if (pageID == 2) {
           // Navigate to Mood Tracker Page
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => MoodTrackerView()));
+        } else if (pageID == 3) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => HistoryPage(BasicHistoryPresenter(),
+                    title: 'HISTORY', key: const Key('HISTORY'))),
+          );
+        } else if (pageID == 4) {
           Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => MoodTrackerView()
-              )
-          );
-        }else if(pageID == 3){
+                  builder: (context) =>
+                      JournalView(presenter: JournalPresenter())));
+        } else if (pageID == 5) {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => RewardsView()));
+        } else if (pageID == 6) {
           Navigator.push(
             context,
-              MaterialPageRoute(builder: (context) => HistoryPage(BasicHistoryPresenter(), title:'HISTORY',key: const Key('HISTORY'))),
-          );
-
-        }else if(pageID == 4){
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => JournalView(presenter: JournalPresenter()))
-          );
-        } else if(pageID == 5){
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => RewardsView())
-          );
-        } else if(pageID == 6){
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SelfcarePage(BasicSelfcarePresenter(), title:'SELFCARE',key: const Key('SELFCARE'))),
+            MaterialPageRoute(
+                builder: (context) => SelfcarePage(BasicSelfcarePresenter(),
+                    title: 'SELFCARE', key: const Key('SELFCARE'))),
           );
         }
-
-
-
       },
       icon: Icon(icon),
       label: Text(label),
       //dictates the size of the button
       style: ElevatedButton.styleFrom(
-        minimumSize: const Size(150,75),
+        minimumSize: const Size(150, 75),
       ),
     );
   }
 }
-
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Text('Home Page'),
-          backgroundColor: Colors.deepPurple.shade200,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<ProfileScreen>(
+                  builder: (context) => ProfileScreen(
+                    appBar: AppBar(
+                      title: const Text('Profile'),
+                    ),
+                    actions: [
+                      SignedOutAction((context) {
+                        Navigator.of(context).pop();
+                      })
+                    ],
+                  ),
+
+                ),
+              );
+            },
+          )
+        ],
+        automaticallyImplyLeading: false,
+        title: Text('Home Page'),
+        backgroundColor: Colors.deepPurple.shade200,
       ),
       body: Center(
         child: Column(
@@ -106,15 +123,21 @@ class HomePage extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Text('Ease your stress',
-            style: TextStyle(
-              fontSize: 20,
-            ),
+            Text(
+              'Ease your stress',
+              style: TextStyle(
+                fontSize: 20,
+              ),
             ),
 
-            SizedBox(height: 40,), //space between title and buttons
+            SizedBox(
+              height: 40,
+            ), //space between title and buttons
             // Planner Button
-            Button(icon: Icons.calendar_month_outlined, label: 'Planner', pageID: 1),
+            Button(
+                icon: Icons.calendar_month_outlined,
+                label: 'Planner',
+                pageID: 1),
             SizedBox(height: 20), // Space between buttons
             //Mood Tracker Button
             Button(icon: Icons.tag_faces, label: 'Mood Tracker', pageID: 2),
