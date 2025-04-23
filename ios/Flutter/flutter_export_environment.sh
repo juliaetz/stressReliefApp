e34bd67927c2d3ee0a0a48bd8730661e0ff21258
Angela Danger <dange084@d.umn.edu>
2025-04-23 17:04:37 -0500

    fix gradle error

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.27.3/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/benponto/Desktop/cs3541GroupProject/stress_managment_app"
+export "FLUTTER_ROOT=C:\Users\angel\Downloads\flutter_windows_3.29.2-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\angel\Desktop\CS 3541 SOFTWARE ENG\grpPrj-spring2025"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
