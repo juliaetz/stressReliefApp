55d46522df91baad618b8f73e45daab66259905b
Andrew Martini <mart6482@d.umn.edu>
2025-04-28 21:21:33 -0500

    Added an Id field to the Journal model to assist in the delete function.

--- lib/presenter/journal_presenter.dart
+++ lib/presenter/journal_presenter.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import '../model/journal_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../firebase_logic.dart';
@@ -19,23 +21,9 @@ class JournalPresenter {
         final userDocRef = await getUserDocument();
         yield* userDocRef.collection('entries').snapshots().map((snapshot) {
             return snapshot.docs.map((doc) =>
-                Entry.fromMap(doc.data() as Map<String, dynamic>)).toList();
+                Entry.fromMap(doc.data() as Map<String, dynamic>, doc.id)).toList();
         });
     }
 
 
-
-
-
-
-//creates a new journal entry
-/*void addEntry(String text){
-        //creates a JournalModel object and adds it to the list
-        Entry entry = Entry(text: text, date: DateTime.now().toString());
-        entries.add(entry);
-
-        //logs the journal entry being added, remove after testing is done
-        print('Journal Entry Saved: ${entry.text} at ${entry.date}');
-
-    }*/
 }
